c4ad2a4d3ebc76e256dd31b9978445959d0bd4e0
Yanga93 <yangazukelwa@gmail.com>
2016-11-07 14:10:21 +0200

    I managed to stop repeating

---
+++ function_intro.js
@@ -0,0 +1,16 @@
+var printDetails = function(value){
+  console.log(value);
+  console.log(typeof value);
+}
+
+printDetails('this is a string');
+printDetails(100);
+printDetails(false);
+
+var isWeekday = function (day) {
+
+}
+
+function isWeekday (day) {
+
+}
