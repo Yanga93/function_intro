cb9906e683b825421619e952b1edb2e292ee3ae2
Yanga93 <yangazukelwa@gmail.com>
2016-11-07 13:55:08 +0200

    Managed to print favourite sport

---
+++ favourite_sport.js
@@ -0,0 +1,11 @@
+var name ="David Blankit";
+
+var favSpot = function(){
+   var favouriteSport = "Soccer";
+    console.log(name + "'s favourite sport is " + favouriteSport);
+  };
+
+  favSport();
+
+  console.log(favoriteSport);
+  
