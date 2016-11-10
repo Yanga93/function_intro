7b6b9bd7f964013ac7f16fcd65d18528eef77683
Yanga93 <yangazukelwa@gmail.com>
2016-11-10 14:41:59 +0200

    Returns true if a regNum is for Bellville otherwise returns false

---
+++ isFromBellville.js
@@ -0,0 +1,7 @@
+var isFromBellville = function(regNum){
+  var location = regNum.startsWith('CY');
+      console.log(location);
+        return location;
+}
+
+  isFromBellville('CY'); 'True','False';
