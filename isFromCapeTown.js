ebf951fea76321a98d5274af4ff0d282c858eb2a
Yanga93 <yangazukelwa@gmail.com>
2016-11-10 15:11:29 +0200

    Returns true if a regNum is for CP otherwise returns false

---
+++ isFromCapeTown.js
@@ -0,0 +1,7 @@
+var isFromCapeTown = function(regNum){
+  var location = regNum.startsWith('CA');
+      console.log(location);
+        return location;
+}
+
+  isFromCapeTown('CA'); 'True','False';
