a31411f6ff49206c31407a200d93acaf68f0ec24
Yanga93 <yangazukelwa@gmail.com>
2016-11-11 11:12:03 +0200

    Returns true if a regNum is for GP, otherwise returns false

--- isFromGauteng.js
+++ isFromGauteng.js
@@ -1,7 +1,7 @@
+const assert = require('assert');
 var isFromGauteng = function(regNum){
   var location = regNum.endsWith('GP');
       console.log(location);
         return location;
 }
-
-  isFromGauteng('GP'); 'True','False';
+  assert.equal(isFromGauteng('GP'), true);
